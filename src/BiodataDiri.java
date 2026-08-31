d2ed6e2c41b4a989a43405badbf68fdd084ef2a4
jaya97-py <siahaanjaya131@gmail.com>
2026-08-31 17:14:52 +0700

    asal daerah

--- src/BiodataDiri.java
+++ src/BiodataDiri.java
@@ -2,7 +2,9 @@ public class BiodataDiri {
     public static void main(String[] args ){
         String nama = "Frenky jul prima jaya waruwu";
         String nim = "2582025";
-        System.out.println("nama : " + nama);
-        System.out.println("nim : " + nim);
+        String asalDaerah = "Nias";
+        System.out.println("Nama            : " + nama);
+        System.out.println("Nim             : " + nim);
+        System.out.println("Asal Daerah     : " + asalDaerah);
     }
 }
