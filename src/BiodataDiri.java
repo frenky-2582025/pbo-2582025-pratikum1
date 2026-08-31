57975f9a2d948537cb7b87966427c2106870cdc0
jaya97-py <siahaanjaya131@gmail.com>
2026-08-31 17:46:32 +0700

    target semester ini

--- src/BiodataDiri.java
+++ src/BiodataDiri.java
@@ -4,9 +4,11 @@ public class BiodataDiri {
         String nim = "2582025";
         String asalDaerah = "Nias";
         String alasanmemilihProdi = "karena prospek pekerjaan yang saya suka sesuai dan juga prospek perkerjaannya luas";
+        String targetsemesterIni = "Ingin menguasai bahasa pemograma java";
         System.out.println("Nama                    : " + nama);
         System.out.println("Nim                     : " + nim);
         System.out.println("Asal Daerah             : " + asalDaerah);
         System.out.println("Alasan Memilih Prodi    : " + alasanmemilihProdi);
+        System.out.println("Target Semester Ini     : " + targetsemesterIni);
     }
 }
