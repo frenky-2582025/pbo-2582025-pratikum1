40e1e1e19d753c93b5c35d6180ad092aad648b80
jaya97-py <siahaanjaya131@gmail.com>
2026-08-31 17:03:16 +0700

    Biodata Diri

---
+++ src/BiodataDiri.java
@@ -0,0 +1,8 @@
+public class BiodataDiri {
+    public static void main(String[] args ){
+        String nama = "Frenky jul prima jaya waruwu";
+        String nim = "2582025";
+        System.out.println("nama : " + nama);
+        System.out.println("nim : " + nim);
+    }
+}
